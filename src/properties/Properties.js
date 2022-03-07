2fea91b67fc91f50fea9a8bbd3ca68847b9ea44c
Алексей <defers86@gmail.com>
2022-03-07 20:56:48 +0300

    s

--- src/properties/Properties.js
+++ src/properties/Properties.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom"
 
 export const getApiUrl = () => {
 
-    const API_URL = "http://localhost:8080/";
+    //const API_URL = "http://localhost:8080/";
+    const API_URL = "https://light-tasks.herokuapp.com/"; 
 
     return API_URL;
 
